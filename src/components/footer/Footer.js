16d4a79e9804611030707a1b7d9b263b03ea911b
chan <chan.jor.zz@gmail.com>
2022-12-24 14:11:25 +0900

    add css style

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,21 +1,170 @@
+import styled, { css } from "styled-components";
+import { BsGithub } from "react-icons/bs";
+import { FiGithub } from "react-icons/fi";
+import { BsFacebook, BsInstagram, BsYoutube } from "react-icons/bs";
+import { SiNaver, SiNotion } from "react-icons/si";
+
 const Footer = () => {
+  const chan = "https://github.com/front-chan";
+  const hayoung = "https://github.com/hannahcodes0";
+  const saeyeon = "https://github.com/saeyeonKim";
+  const yeol = "https://github.com/misracis2";
+  const jaeha = "https://github.com/wogk9854";
+  const jongyeol = "https://github.com/jongyeol2";
+  const gyumin = "https://github.com/starMinK";
+  const notion = "https://www.notion.so/6-21181e915241465c92495075706dbe3c";
+
+  const facebook = "https://www.facebook.com/daangn";
+  const instagram = "https://www.instagram.com/daangnmarket/";
+  const youtube = "https://www.youtube.com/channel/UC8tsBsQBuF7QybxgLmStihA";
+  const blog = "https://blog.naver.com/daangn";
+
   return (
-    <div style={{ ...FooterStyles }}>
-      <span>copyright ©️ 10기 C반 6조 FOOTER | BE || FE |</span>
-    </div>
+    <StDiv footer_box>
+      <div>
+        <StP copy_right>copyright ©️ 10기 C반 6조</StP>
+        <StDiv back_front_txt>
+          <p>BackEnd</p>
+          <p>FrontEnd</p>
+        </StDiv>
+        <StButton
+          onClick={() => {
+            window.open(yeol);
+          }}
+        >
+          <BsGithub size="25" color="#868b94" />
+          <StP>이승열</StP>
+        </StButton>
+        <StButton
+          onClick={() => {
+            window.open(jaeha);
+          }}
+        >
+          <BsGithub size="25" color="#868b94" />
+          <StP>최재하</StP>
+        </StButton>
+        <StButton
+          onClick={() => {
+            window.open(jongyeol);
+          }}
+        >
+          <BsGithub size="25" color="#868b94" />
+          <StP>유종열</StP>
+        </StButton>
+        <StButton
+          onClick={() => {
+            window.open(gyumin);
+          }}
+        >
+          <BsGithub size="25" color="#868b94" />
+          <StP>김규민</StP>
+        </StButton>
+        <StButton
+          onClick={() => {
+            window.open(chan);
+          }}
+        >
+          <FiGithub size="25" color="#868b94" />
+          <StP>최신영</StP>
+        </StButton>
+        <StButton
+          onClick={() => {
+            window.open(hayoung);
+          }}
+        >
+          <FiGithub size="25" color="#868b94" />
+          <StP>김하영</StP>
+        </StButton>
+        <StButton
+          onClick={() => {
+            window.open(saeyeon);
+          }}
+        >
+          <FiGithub size="25" color="#868b94" />
+          <StP>김세연</StP>
+        </StButton>
+        <StButton
+          onClick={() => {
+            window.open(notion);
+          }}
+        >
+          <SiNotion size="25" color="#868b94" />
+          <StP>6조 노션</StP>
+        </StButton>
+      </div>
+      <div>
+        <StButton
+          onClick={() => {
+            window.open(facebook);
+          }}
+        >
+          <BsFacebook size="25" color="#868b94" />
+        </StButton>
+        <StButton
+          onClick={() => {
+            window.open(instagram);
+          }}
+        >
+          <BsInstagram size="25" color="#868b94" />
+        </StButton>
+        <StButton
+          onClick={() => {
+            window.open(youtube);
+          }}
+        >
+          <BsYoutube size="25" color="#868b94" />
+        </StButton>
+        <StButton
+          onClick={() => {
+            window.open(blog);
+          }}
+        >
+          <SiNaver size="25" color="#868b94" />
+        </StButton>
+      </div>
+    </StDiv>
   );
 };
 
-const FooterStyles = {
-  width: "100%",
-  height: "100px",
-  display: "flex",
-  background: "burlywood",
-  color: "#0a0327",
-  alignItems: "center",
-  justifyContent: "center",
-  fontSize: "22px",
-  textAlign: "center",
-};
+const StDiv = styled.div`
+  ${(props) =>
+    props.footer_box &&
+    css`
+      max-width: 1200px;
+      width: 95%;
+      height: 150px;
+      display: flex;
+      color: #868b94;
+      align-items: flex-start;
+      justify-content: space-between;
+      font-size: 14px;
+      margin: 0 auto;
+      padding: 12px 16px;
+    `}
+  ${(props) =>
+    props.back_front_txt &&
+    css`
+      display: flex;
+      justify-content: flex-start;
+      margin-left: 10px;
+      gap: 130px;
+    `}
+`;
+
+const StButton = styled.button`
+  background-color: transparent;
+  border: 0;
+  cursor: pointer;
+`;
+
+const StP = styled.p`
+  margin: 0;
+  color: #868b94;
+  ${(props) =>
+    props.copy_right &&
+    css`
+      margin-left: 10px;
+    `}
+`;
 
 export default Footer;
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,13 +1,37 @@
 import { useNavigate } from "react-router-dom";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 const Header = () => {
   const navigate = useNavigate();
 
   return (
-    <StDiv>
-      <StSpan onClick={() => navigate("/")}>바니바니 당근당근</StSpan>
-
+    <StDiv nav_main>
+      <div>
+        <StSpan nav_logo onClick={() => navigate("/")}>
+          <StImg
+            src="https://cdn.discordapp.com/attachments/1037267111585792020/1055829632592658462/images.png"
+            alt="logo"
+          />
+          바니바니 당근마켓
+        </StSpan>
+      </div>
+      <StDiv nav_category>
+        <StDiv cate_gory>
+          <StSpan cate_txt active_txt onClick={() => navigate("/")}>
+            중고거래
+          </StSpan>
+          <StSpan cate_txt onClick={() => navigate("/post")}>
+            글쓰기
+          </StSpan>
+          <StSpan cate_txt onClick={() => navigate("/chat")}>
+            채팅하기
+          </StSpan>
+        </StDiv>
+        <StDiv log_sign>
+          <StSpan onClick={() => navigate("/login")}>로그인</StSpan>
+          <StSpan onClick={() => navigate("/signup")}>회원가입</StSpan>
+        </StDiv>
+      </StDiv>
       {/* 토큰이 있으면 로그아웃으로 버튼 변경(누르면 쿠키삭제) / 토큰 없으면 로그인 버튼 */}
       {/* {!localStorage.getItem("id") ? (
           <StSpan onClick={() => navigate("/login")}>Login</StSpan>
@@ -28,21 +52,67 @@ const Header = () => {
 };
 
 const StDiv = styled.div`
-  background-color: burlywood;
-  height: 65px;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 5px 5px;
-  padding-right: 30px;
-  padding-left: -20px;
+  ${(props) =>
+    props.nav_main &&
+    css`
+      max-width: 1200px;
+      width: 95%;
+      height: 65px;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      padding: 12px 16px;
+      margin: 0 auto;
+    `}
+  ${(props) =>
+    props.nav_category &&
+    css`
+      display: flex;
+      gap: 60px;
+      align-items: baseline;
+    `}
+  ${(props) =>
+    props.cate_gory &&
+    css`
+      display: flex;
+      gap: 30px;
+    `}
+  ${(props) =>
+    props.log_sign &&
+    css`
+      display: flex;
+      gap: 15px;
+    `}
 `;
 
 const StSpan = styled.span`
-  color: black;
-  font-size: 30px;
-  font-weight: bold;
   cursor: pointer;
+  ${(props) =>
+    props.nav_logo &&
+    css`
+      color: #f76605;
+      font-size: 30px;
+      font-weight: bold;
+      font-family: "Jua", sans-serif;
+      font-weight: 100;
+      display: flex;
+    `}
+  ${(props) =>
+    props.cate_txt &&
+    css`
+      font-size: 18px;
+      font-weight: bold;
+    `}
+    ${(props) =>
+    props.active_txt &&
+    css`
+      color: #fa6616;
+    `}
+`;
+
+const StImg = styled.img`
+  width: 30px;
+  height: 30px;
 `;
 
 export default Header;
